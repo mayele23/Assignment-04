f69ec97c7afaef7e39231876aad6994a86ce4f7a
Mikyas Ayele <mayele23@Mikyass-MBP.PK5001Z>
2019-02-12 10:53:42 -0800

    webpage opens an alert to square numbers as well as a functioning button that multiplies two numbers given by the user

---
+++ event-handler.js
@@ -0,0 +1,17 @@
+    let number = prompt('Please Enter a Number to Square:')
+
+    function square(numberToSquare) {
+      return numberToSquare * number;
+    }
+    alert(square(number));
+
+
+    function math() {
+
+      let x, y;
+      x = prompt("Enter a number to multiply");
+      y = prompt("Enter another number")
+      let result = Number(x) * Number(y);
+      alert("The result is" + ' ' + result);
+    }
+    square(x, y);
